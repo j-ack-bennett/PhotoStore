6c0558b94fa4181fabd96f5597d79f2dbcba7c9b
j-ack-bennett <bennettjack44@gmail.com>
2021-04-06 12:58:43 +1200

    added map to create image element

--- components/Photos.jsx
+++ components/Photos.jsx
@@ -1,9 +1,19 @@
-import React from "react"
+import React, { useContext } from "react"
+
+import Image from "./Image"
+import { Context } from "../Context"
+import { getClass } from "../utilities"
 
 const Photos = () => {
+  const {photos} = useContext(Context)
+
+  const images = photos.map((img, i) => (
+    <Image key={img.id} img={img} className={getClass(i)} />
+  ))
+
   return (
     <main className="photos">
-      <h1>Images go here</h1>
+      {images}
     </main>
   )
 }
