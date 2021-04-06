2b98b13b4a04fd6ab41ffb77be255490d197cd6b
j-ack-bennett <bennettjack44@gmail.com>
2021-04-06 12:11:54 +1200

    wrapped App in provider

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom"
 import {BrowserRouter as Router} from "react-router-dom"
 
 import App from "./App"
+import ContextProvider from "./Context"
 
 ReactDOM.render(
-  <Router>
+  <ContextProvider>
+    <Router>
       <App />
-  </Router>, 
+    </Router>
+  </ContextProvider>, 
   document.getElementById("root")
 )
