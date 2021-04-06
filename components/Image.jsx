96a09df1784b008f65062a26b4f1f0b116bacdb1
j-ack-bennett <bennettjack44@gmail.com>
2021-04-06 13:13:43 +1200

    added state for image hovering

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,8 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Image = props => {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
-    <div className={`${props.className} image-container`}>
+    <div 
+      className={`${props.className} image-container`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img src={props.img.photoUrl} className="image-grid" />
     </div>
   )
