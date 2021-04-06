5c2ae4d1d01be0209851075c13f70466dd45fe25
j-ack-bennett <bennettjack44@gmail.com>
2021-04-06 12:35:04 +1200

    fetching images from api

--- Context.js
+++ Context.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Context = React.createContext()
 
 const ContextProvider = props => {
   const [photos, setPhotos] = useState([])
+  const photoUrl = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    
+  useEffect(() => {
+    fetch(photoUrl)
+      .then(res => res.json())
+      .then(data => setPhotos(data))
+  }, [])
 
   return (
     <Context.Provider value={{photos}}>
