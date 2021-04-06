0bcf098f94cdf7983a1464eda87f22d0fda3453b
j-ack-bennett <bennettjack44@gmail.com>
2021-04-06 12:11:37 +1200

    changed header name

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header>
       <Link to="/">
-        <h2>Pic Some</h2>
+        <h2>Photo Store</h2>
       </Link>
       <Link to="cart">
         <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
