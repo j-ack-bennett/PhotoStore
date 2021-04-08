0d5f18165a06958636d9377da3d6a81eb6064ac3
j-ack-bennett <bennettjack44@gmail.com>
2021-04-08 15:59:32 +1200

    added function to place order

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,16 +1,26 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 
 import { Context } from "../Context"
 import CartItem from "./CartItem"
 
 const Cart = () => {
-  const {cartItems} = useContext(Context)
+  const {cartItems, emptyCart} = useContext(Context)
+  const [text, setText] = useState("Place Order")
   const totalCost = 9.99 * cartItems.length
   const displayTotalCost = totalCost.toLocaleString("en-NZ", {style: "currency", currency: "NZD"})
 
   const cartItemElements = cartItems.map(item => (
     <CartItem key={item.id} item={item} />
   ))
+  
+  const placeOrder = () => {
+    setText("Ordering...")
+    setTimeout(() => {
+      console.log("Order Placed!")
+      setText("Place Order")
+      emptyCart()
+    }, 3000)
+  }
 
   return (
     <main className="cart-page">
@@ -18,7 +28,7 @@ const Cart = () => {
       {cartItemElements}
       <p className="total-cost">Total: {displayTotalCost}</p>
       <div className="order-button">
-        <button>Place Order</button>
+        <button onClick={placeOrder}>{text}</button>
       </div>
     </main>
   )
