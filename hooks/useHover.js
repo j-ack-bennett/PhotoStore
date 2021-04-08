d85a0ec897463ac9b79327aa5025a393e75e3959
j-ack-bennett <bennettjack44@gmail.com>
2021-04-08 16:50:00 +1200

    created custom hook file for hovering

---
+++ hooks/useHover.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from "react"
+
+const useHover = () => {
+  const [isHovered, setIsHovered] = useState(false)
+  const ref = useRef(null)
+  
+  const enter = () => {
+    setIsHovered(true)
+  }
+  
+  const leave = () => {
+    setIsHovered(false)
+  }
+  
+  useEffect(() => {
+    ref.current.addEventListener("mouseenter", enter)
+    ref.current.addEventListener("mouseleave", leave)
+    
+    return () => {    
+      ref.current.removeEventListener("mouseenter", enter)
+      ref.current.removeEventListener("mouseleave", leave)
+    }
+  }, [])
+  
+  return [isHovered, ref]
+}
+
+export default useHover
