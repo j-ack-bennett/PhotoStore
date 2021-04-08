b16c629b33482657768a25cb6c1cdacd48e4d5ab
j-ack-bennett <bennettjack44@gmail.com>
2021-04-08 15:05:37 +1200

    put spacing in imports

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-import {Context} from "../Context"
+import { Context } from "../Context"
 
 const Header = () => {
   const {cartItems} = useContext(Context)
