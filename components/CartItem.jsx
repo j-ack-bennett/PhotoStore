227f541e71832e23eabae05cf86b1f8f0ce4c137
j-ack-bennett <bennettjack44@gmail.com>
2021-04-08 16:50:26 +1200

    updated to use custom hook

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useState } from "react"
 import PropTypes from "prop-types"
 
 import { Context } from "../Context"
+import useHover from "../hooks/useHover"
 
 const CartItem = props => {
   const {removeFromCart} = useContext(Context)
-  const [isHovered, setIsHovered] = useState(false)
+  const [isHovered, ref] = useHover()
 
   const trashIcon = isHovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
   
@@ -14,8 +15,7 @@ const CartItem = props => {
       <i 
         className={trashIcon} 
         onClick={() => removeFromCart(props.item.id)}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        ref={ref}
       >
       </i>
       <img src={props.item.url} width="130px" />
