a433a810d3da3b0d0389517d561e0e2e6b8d1d24
j-ack-bennett <bennettjack44@gmail.com>
2021-04-08 16:26:50 +1200

    trash icons fill if hovered over

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -1,13 +1,22 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 
 import { Context } from "../Context"
 
 const CartItem = props => {
   const {removeFromCart} = useContext(Context)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const trashIcon = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
   
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line" onClick={() => removeFromCart(props.item.id)}></i>
+      <i 
+        className={trashIcon} 
+        onClick={() => removeFromCart(props.item.id)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+      </i>
       <img src={props.item.url} width="130px" />
       <p>$9.99</p>
     </div>
